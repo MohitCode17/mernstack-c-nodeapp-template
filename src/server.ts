2b4c0d955b7950ff9819025747e37c71ea6d59b5
MohitCode17 <mohit.codes17@gmail.com>
2024-12-19 23:48:46 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 function welcome(name: string) {
+  // const username = {
+  //   fname: "Mohit",
+  // }
+
+  // const firstName = username.fname;
+  // console.log(firstName);
   console.log("Welcome" + name);
 }
 
